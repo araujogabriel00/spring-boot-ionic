862cd41574b1d3bef5022aab20ae86cdba8de224
gabrielaraujo00 <gabriel.araujos@sempreceub.com>
2020-12-11 19:19:25 -0300

    Cliente, TipoCliente, telefones e endereços

---
+++ src/main/java/com/workshop/enums/TipoCliente.java
@@ -0,0 +1,48 @@
+package com.workshop.enums;
+///Controle do codigo atribuido a cada valor
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "PESSOA FISICA"), PESSOAJURIDA(2, "PESSOA JURIDA");
+
+	private int cod;
+	private String descricao;
+
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	///
+	public static TipoCliente toEnum(Integer cod) {
+
+		if (cod == null) {
+			return null;
+		}
+
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+
+		}
+
+		throw new IllegalArgumentException("Id invalido: " + cod);
+	}
+
+}
