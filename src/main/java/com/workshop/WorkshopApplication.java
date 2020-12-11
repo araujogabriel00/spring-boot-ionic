6f8800ef23e315e2e3b9e1e1242f49259bf22f87
gabrielaraujo00 <gabriel.araujos@sempreceub.com>
2020-12-11 15:31:43 -0300

    Operção de instaciação

--- src/main/java/com/workshop/WorkshopApplication.java
+++ src/main/java/com/workshop/WorkshopApplication.java
@@ -1,13 +1,46 @@
 package com.workshop;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WorkshopApplication {
+import com.workshop.entitites.Categoria;
+import com.workshop.repositories.CategoriaRepo;
 
+///CLASSE DE APLICAÇÃO
+///ESTANCIAR AS CLASSES REPOSITORIOS QUE IRÃO UTILIZAR OS DADOS
+///IMPLEMENTAR O COMMANDLINERUNNER
+///A CLASSE DE REPOSITORIO DEVERÁ SALVAR OS OBJS INSTANCIADOS
+@SpringBootApplication
+public class WorkshopApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepo categoriarepo;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriarepo.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+		
+		
+		
+	
+
+		
+		
 	}
 
 	
