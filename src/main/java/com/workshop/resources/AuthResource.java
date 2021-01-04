652b6d00ef90c5b73d6d2414f5ed99b0a3944ea8
araujogabriel00 <gabriel.araujos@sempreceub.com>
2021-01-04 19:42:35 -0300

    Refresh token

---
+++ src/main/java/com/workshop/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.workshop.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workshop.resources.utils.JWTUtil;
+import com.workshop.security.UserSS;
+import com.workshop.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jWTUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jWTUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+
+}
